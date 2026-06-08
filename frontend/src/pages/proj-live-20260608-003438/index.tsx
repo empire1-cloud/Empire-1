9b77bdabf9154faffc298aad8fc8670bf9b2f2b6
Manda mora <shiestybizz113@gmail.com>
2026-06-07 17:34:48 -0700

    cofounder: add frontend/src/pages/proj-live-20260608-003438/index.tsx

---
+++ frontend/src/pages/proj-live-20260608-003438/index.tsx
@@ -0,0 +1,9 @@
+import { Main } from "@/components/proj-live-20260608-003438/Main";
+
+export default function Proj-live-20260608-003438Page() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Main />
+    </div>
+  );
+}
