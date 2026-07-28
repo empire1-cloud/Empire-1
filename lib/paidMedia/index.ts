10bd88c2117733a13bb3026a7ed6f62e9d0b2bc3
Manda mora <shiestybizz113@gmail.com>
2026-07-28 15:19:58 -0700

    Export paid media governance services

---
+++ lib/paidMedia/index.ts
@@ -0,0 +1,15 @@
+export { providerStatus, requireOperator } from './core';
+export {
+  amendBudget,
+  approveCreative,
+  authorizeBudget,
+  createPaidMediaCampaign,
+  paidMediaSummary,
+  revokePaidMediaCampaign,
+} from './campaigns';
+export {
+  launchPaidMediaCampaign,
+  pausePaidMediaCampaign,
+  revokeProviderCampaigns,
+  syncPaidMediaCampaign,
+} from './provider';
